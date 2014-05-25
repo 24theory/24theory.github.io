c6c65f97a144c5a09fc2a47abca65a084d3d2392
Cheng Chang <visionc@gmail.com>
2014-05-25 15:51:59 -0400

    Update challenge.js

--- weixin/1024/challenge.js
+++ weixin/1024/challenge.js
@@ -392,8 +392,8 @@ function start_game(){
 
 
 	rect_quit  = new Array( (marg_ratio + 2)* width /3, (marg_ratio/2 + 2) * height /3, (1- 2 * marg_ratio) * width /3, (1- 2 * marg_ratio) * height /6);
-	rect_skip  = new Array( (marg_ratio + 2)* width /3, (marg_ratio/2 + 2.5) * height /3, (1- 2 * marg_ratio) * width /3, (1- 2 * marg_ratio) * height /6);
-
+	rect_skip  = new Array( width * ( 1- (1-N1*2) * quit_ratio), marg_ratio * width * N1, (1-N1*2) * quit_ratio* width, (1-N1*2) * quit_ratio* width);
+	
 	rect_all.push(rect_undo, rect_redo, rect_skip, rect_quit); // rect 8,9,10,11
 	rect_all.push(rect_undo, rect_redo, rect_skip, rect_quit, rect_addall, rect_multiall); // rect 8,9,10,11
 		
