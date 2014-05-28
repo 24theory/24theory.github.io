99c4f6eb4de232cfbaf0621ce345e833c1c5e93e
Cheng Chang <changch@deshaw.com>
2014-05-27 22:04:05 -0400

    a

--- weixin/1024/challenge.js
+++ weixin/1024/challenge.js
@@ -223,8 +223,7 @@ function ongame(regionID){
 }
 
 function init_game(game_input){
-	var a = ["ddd", "dd"];
-	alert(a[1]);
+	 
 	game_str =game_input;  	
 	
 	canvas_ele   =  document.getElementById(canvasID) ;
