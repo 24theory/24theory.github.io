ddd73f48b247a00df83b81cec7361fcc599504ec
Cheng Chang <visionc@gmail.com>
2014-07-13 23:01:55 -0400

    Update challenge.js

--- weixin/today/challenge.js
+++ weixin/today/challenge.js
@@ -14,7 +14,7 @@ var percent_w_init = 0.4, percent_h_init  = 0.4;
 var xywh_init ,  xywh_init1;
 var solved = 0, unsolved = 1362, totalQ = 1362;
 var this_quad;
-var time_now = 0, time_left = 0, time_tick = 0, game_tick = 0;  // time_tick is .01 second
+var time_now = 0, time_left = 0, time_tick = 0, game_tick = 0, game_tick1;  // time_tick is .01 second
 var pause_tick = 0, pause_total = 500, pause_total1;
 var coffee_tick = 0, coffee_total = 60001, coffee_num = 40, coffee_waiting = 0; 
 var now, before;
@@ -64,6 +64,7 @@ function gameover(){
 	document.getElementById("gamesub").time_array.value =  arraytostring(time_array);
 	document.getElementById("gamesub").submit();
 */
+	game_tick1 = game_tick;
 	set_title();
 //	init_game(gamestr);
 }
