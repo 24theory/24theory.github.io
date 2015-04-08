1d5077eff0ec81931aa9ee9521a13f9018984be7
Cheng Chang <visionc@gmail.com>
2015-04-08 17:27:09 -0400

    Update demo.js

--- weixin/today/demo.js
+++ weixin/today/demo.js
@@ -1,8 +1,7 @@
  
 var canvasID      = "canvasID";
 var canvas_ele;
-var canvas_cts; 
- 
+var width, height;
 function init_game(all_inputs ){
   	canvas_ele   =  document.getElementById(canvasID) ;
 	canvas_ele.addEventListener("click", onclick1, false);
@@ -10,5 +9,27 @@ function init_game(all_inputs ){
 	canvas_ele.focus();
 	width  = canvas_ele.width;
 	height = canvas_ele.height;
-  
+	canvas_cts = canvas_ele.getContext("2d");
+	game_draw(0);
 } 
+function game_draw(isclock){ // 0) status1, 1) game_type, 2) time, time_left, 3) solved, unsolved, 4) this game history
+	if (status1 == 0){
+		draw_rect(Array(0,0, width, height), "#fff", 0, "#000"); // clean the whole region
+		draw_text (Array(xywh_init1[0] + xywh_init1[2] /5, xywh_init1[1] + xywh_init1[3] /1.9),game_type1, "#fff",  "bold " + Math.round(xywh_init1[2]/10) +"px sans-serif");
+		
+	return;
+	}
+}
+function draw_rect(rect1, color1, border_wid1, color2 ){
+	if (color1 !="fff")
+	{
+		canvas_cts.fillStyle = color1;   // button color
+		canvas_cts.fillRect (Math.round(rect1[0]), Math.round(rect1[1]), Math.round(rect1[2]), Math.round(rect1[3]));	
+	}
+	else
+		canvas_cts.clearRect (Math.round(rect1[0]), Math.round(rect1[1]), Math.round(rect1[2]), Math.round(rect1[3]));	
+	canvas_cts.lineWidth   = Math.floor(border_wid1);
+	canvas_cts.strokeStyle  = color2;   // button color
+	if (border_wid1>0)
+		canvas_cts.strokeRect(Math.round(rect1[0] + border_wid1/ 2), Math.round(rect1[1] + border_wid1/2), Math.round(rect1[2] - border_wid1), Math.round(rect1[3] - border_wid1));
+}
