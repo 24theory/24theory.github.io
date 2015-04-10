d93827db436407532fd539204ef5523e7a8ebe63
Cheng Chang <visionc@gmail.com>
2015-04-09 22:47:39 -0400

    dd

--- weixin/today/data.js
+++ weixin/today/data.js
@@ -1,4 +1,4 @@
 var canw = 500, canh = 500; 
 var xnum = 4, ynum = 5; 
-var x_disp = Math.floor(0.5 * canvw / (xnum+1)), Math.floor(y_disp = 0.5 * canvh / (ynum+1));
+var x_disp = Math.floor(0.5 * canvw / (xnum+1)), y_disp =Math.floor( 0.5 * canvh / (ynum+1));
 var x_size = x_disp * 2, y_size = y_disp * 2; 
