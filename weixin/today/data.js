1f1aaa37a1c9ad98617eb3d3fb949701b92c139a
Cheng Chang <visionc@gmail.com>
2015-04-09 22:54:44 -0400

    dd

---
+++ weixin/today/data.js
@@ -0,0 +1,5 @@
+
+var canw = 500, canh = 500; 
+var xnum = 4, ynum = 5; 
+var x_disp = Math.floor(0.5 * canw / (xnum+1)), y_disp =Math.floor( 0.5 * canh / (ynum+1));
+var x_size = x_disp * 2, y_size = y_disp * 2; 
--- weixin/today/demo.js
+++ weixin/today/demo.js
@@ -3,11 +3,6 @@ var canvas_ele;
 var width, height;
 var status1;
 
-var canw = 500, canh = 500; 
-var xnum = 4, ynum = 5; 
-var x_disp = Math.floor(0.5 * canw / (xnum+1)), y_disp =Math.floor( 0.5 * canh / (ynum+1));
-var x_size = x_disp * 2, y_size = y_disp * 2; 
-
 function init_game( ){
   	canvas_ele   =  document.getElementById(canvasID) ;
 //	canvas_ele.addEventListener("click", onclick1, false);
