c1056371ada110f8e0dd8f7f5f3b5f9a0239cb59
Cheng Chang <visionc@gmail.com>
2015-04-11 11:22:23 -0400

    dd

--- weixin/today/demo.js
+++ weixin/today/demo.js
@@ -50,14 +50,14 @@ function draw_convex(x_array,y_array,xy_vec, color1){
         if(x_array[ii] < x_min){x_min = x_array[ii];}
         if(x_array[ii] > x_max){x_max = x_array[ii];}
         if(y_array[ii] < y_min){y_min = y_array[ii];}
-        if(y_array[ii] > y_max){y_min = y_array[ii];}
+        if(y_array[ii] > y_max){y_max = y_array[ii];}
     }
     x_min+=xy_vec[0];
     x_max+=xy_vec[0];
     y_min+=xy_vec[1];
     y_max+=xy_vec[1];
     var rect=[x_min * x_size + x_disp - bord, y_min * y_size + y_disp - bord,(x_max-x_min)*x_size -2* bord, (y_max-y_min)*y_size -2* bord ];
-    alert(rect[0]+ " " +rect[1]+ " " +rect[2]+ " " +rect[3]+ " " );
+   // alert(rect[0]+ " " +rect[1]+ " " +rect[2]+ " " +rect[3]+ " " );
     draw_rect(rect, color1, x_disp, color1);
 }
 
