c5cba81cc30d3b7f26f84a98298d8ca01ef7f5e1
Cheng Chang <visionc@gmail.com>
2015-04-11 10:53:12 -0400

    dd

--- weixin/today/demo.js
+++ weixin/today/demo.js
@@ -40,23 +40,7 @@ function game_draw(isclock){ // 0) status1, 1) game_type, 2) time, time_left, 3)
 	}
 }
 
-function draw_convex(x_array,y_array,xy_vec, color1){
-    var x_min = 100; var x_max = -100;
-    var y_min = 100; var y_max = -100;
-    for (ii=0; ii < x_array.length(); ii++)
-    {
-        if(x_array[ii] < x_min){x_min = x_array[ii];}
-        if(x_array[ii] > x_max){x_max = x_array[ii];}
-        if(y_array[ii] < y_min){y_min = y_array[ii];}
-        if(y_array[ii] > y_max){y_min = y_array[ii];}
-    }
-    x_min+=xy_vec[0];
-    x_max+=xy_vec[0];
-    y_min+=xy_vec[1];
-    y_max+=xy_vec[1];
-    array rect=[x_min * x_size + x_disp - bord, y_min * y_size + y_disp - bord,(x_max-x_min)*x_size -2* bod, (y_max-y_min)*y_size -2* bod ];
-    draw_rect(rect, color1, -1, color1);
-}
+
 
 function draw_rect(rect1, color1, border_wid1, color2 ){
 	if (color1 !="fff")
