b190fe36f1329d98e09be34331fde1b8683bec1d
Cheng Chang <visionc@gmail.com>
2015-04-12 12:38:46 -0400

    d

--- weixin/today/demo.js
+++ weixin/today/demo.js
@@ -30,6 +30,9 @@ function init_game( ){
 function game_draw(isclock){ // 0) status1, 1) game_type, 2) time, time_left, 3) solved, unsolved, 4) this game history
 	if (status1 == 0){
 		draw_rect(Array(0,0, width, height), "#fff", 0, "#000"); // clean the whole region
+		 x_pos = init_x.slice();
+                y_pos = init_y.slice();
+                            game_draw(1);
 		draw_rect(init_xywh, "#aaa", 0, "#000"); 
 		var textrect =init_xywh.slice(); textrect[1] +=y_disp * 2; 
 		draw_text (textrect, "点击开始看演示", "#fff",  "bold " + Math.round(canw/10) +"px sans-serif");
