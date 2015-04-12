abd16d643e29166c2da5c8db60157237a9dee068
Cheng Chang <visionc@gmail.com>
2015-04-11 22:24:50 -0400

    d

--- weixin/today/data.js
+++ weixin/today/data.js
@@ -1,6 +1,6 @@
 var xnum = 4, ynum = 5; 
-var shape_x =[[1,2],[0] [3]];
-var shape_y =[[0,0],[0] [0]];
+var shape_x =[[1,2],[0] ,[3]];
+var shape_y =[[0,0],[0] ,[0]];
 var init_x =[0,0,0];
 var init_y=[0,0,0];
 var move = [[0,0,0],[1,-1,0][0,1,0]];
