7805b593e8b3a3a2e2bc33fe0c34ccdcc66d0da0
Cheng Chang <visionc@gmail.com>
2015-04-11 22:58:13 -0400

    d

--- weixin/today/demo.js
+++ weixin/today/demo.js
@@ -40,6 +40,7 @@ function game_draw(isclock){ // 0) status1, 1) game_type, 2) time, time_left, 3)
 	    draw_rect(Array(0,0, width, height), "#fff", 0, "#000"); // clean the whole region
 	    draw_text (init_xywh, game_tick, "#000",  "bold " + Math.round(canw/20) +"px sans-serif");
 	      var rect = [x_disp, y_disp, x_size*xnum, y_size * ynum];
+	      draw_rect(rect, "#fff", 2, "#00f" );
 //	   alert(rect);
 //	    canvas_cts.lineWidth   = 2;
 //	   canvas_cts.strokeStyle  = "#00f";   // button color
