da5cd94a80e5fe08fbcf2111d4978be704c23fe6
Cheng Chang <visionc@gmail.com>
2016-03-03 22:51:27 -0500

    dc

--- weixin/beijingpm25/src/plot_line.js
+++ weixin/beijingpm25/src/plot_line.js
@@ -32,10 +32,10 @@ function plot_line(data_vec, labels0, colors, curvenames, para)
 	lineChartData.datasets[ii].data =data_vec[ii]; 
     }
     var ctx = document.getElementById(para.CANVAS_ID).getContext("2d");
-    window.myLine = new Chart(ctx).Line(lineChartData, { 
+    window.myLine = new Chart(ctx).Line(lineChartData, {
 			 scaleOverride : true,
                          scaleSteps : para.scaleSteps,
-                         scaleStepWidth : para.scaleStepWidth*0.5,
+                         scaleStepWidth : para.scaleStepWidth,
                          scaleStartValue : para.scaleStartValue,
                          pointDotRadius : para.pointDotRadius,	    
                          pointDot : para.pointDot,
