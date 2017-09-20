3da3df6560a5ccd740f364c298d2eb1ddd88c4c8
Cheng Chang <visionc@gmail.com>
2017-09-19 22:25:20 -0400

    dd

--- weixin/corp/js/challenge314159.js
+++ weixin/corp/js/challenge314159.js
@@ -43,9 +43,11 @@ var num_ratio = 2/5, marg_ratio =  0.2, rect_thin_width = 3, rect_fat_width = 10
 var quit_ratio = 0.8;
 
 function getname() { 
-    var person = prompt("请输入你的名字:", "");
+    var person = prompt("请输入你的名字(长度<8):", "");
     if (person == null || person == "") {
-         person = prompt("名字不能为空:", "");
+		    if ( person == ""){
+         		person = prompt("名字不能为空(长度<8):", "");
+		}
     } else {
         
     }
